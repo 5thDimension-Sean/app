803839125d293789b92592c6ca1990568545c505
Sean <ss0486034@gmail.com>
2025-11-08 14:06:25 -0500

    Signed-off-by: Sean <ss0486034@gmail.com>

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Render provides the PORT in env
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 4000))  # Render provides the PORT in env
+    app.run(host="127.0.0.1", port=port, debug=True)
